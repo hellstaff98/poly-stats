f1d6b803d2b99d78e2519568012c06a7f1c6f816
hellstaff98 <aleksey_2907@mail.ru>
2026-03-21 19:50:45 +0300

    fixed gitignore

---
+++ frontend/config/build/types/config.ts
@@ -0,0 +1,14 @@
+
+export type BuildMode = 'production' | 'development'
+
+export interface BuildPaths {
+    entry: string,
+    output: string,
+    html: string,
+}
+
+export interface BuildOptions {
+    mode: BuildMode;
+    paths: BuildPaths;
+    isDev: boolean;
+}
